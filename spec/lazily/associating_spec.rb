397f7230c401921297d9c40be23ed934d020b366
Mike Williams <mdub@dogbiscuit.org>
2013-07-10 23:12:57 +1000

    More tests.

--- spec/lazily/associating_spec.rb
+++ spec/lazily/associating_spec.rb
@@ -2,8 +2,6 @@ require "spec_helper"
 
 describe Lazily, "associating" do
 
-  let(:collectionA) { [2,4,6]}
-
   describe ".associate" do
 
     let(:result) do
@@ -12,13 +10,53 @@ describe Lazily, "associating" do
 
     context "with two identical Enumerables" do
 
+      let(:collectionA) { [2,4,6]}
       let(:collectionB) { collectionA }
 
-      it "returns matched pairs" do
+      it "yields matched pairs" do
         expect(result).to eq [
           { :A => [2], :B => [2] },
           { :A => [4], :B => [4] },
           { :A => [6], :B => [6] }
+        ]
+      end
+
+    end
+
+    context "with three identical Enumerables" do
+
+      let(:collectionA) { [1,2]}
+      let(:collectionB) { collectionA }
+      let(:collectionC) { collectionA }
+
+      let(:result) do
+        Lazily.associate(:A => collectionA, :B => collectionB, :C => collectionC).to_a
+      end
+
+      it "yields matched triples" do
+        expect(result).to eq [
+          { :A => [1], :B => [1], :C => [1] },
+          { :A => [2], :B => [2], :C => [2] }
+        ]
+      end
+
+    end
+
+    context "when elements are missing" do
+
+      let(:collectionA) { [1,2] }
+      let(:collectionB) { [2,3] }
+      let(:collectionC) { [1,3] }
+
+      let(:result) do
+        Lazily.associate(:A => collectionA, :B => collectionB, :C => collectionC).to_a
+      end
+
+      it "omits the key for the corresponding enumerable" do
+        expect(result).to eq [
+          { :A => [1], :C => [1] },
+          { :A => [2], :B => [2] },
+          { :B => [3], :C => [3] }
         ]
       end
 
